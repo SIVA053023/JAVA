8a19342d3803bcda970206108995652314d349f1
siva krishna <sivakrishna053936@gmail.com>
2024-07-02 12:41:29 +0530

    test

---
+++ Collection interface/QUEE/PQ/Demo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class PQ{
+    public static void main(String[] args) {
+        
+      PriorityQueue<String> enames=new PriorityQueue<String>();
+      enames.add("rahul");
+      enames.add("modi");
+      enames.add("priyanka");
+      System.out.println(enames);
+   
+      System.out.println(enames.poll());
+      enames.poll();
+      System.out.println(enames);
+      enames.offer("Shiva");
+      System.out.println(enames);
+      System.out.println(enames.peek());
+      System.out.println(enames.element());
+    }
+}
---
+++ Collection interface/QUEE/PQ/Test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class PQ {
+    public static void main(String[] args) {
+        PriorityQueue<String> enames=new PriorityQueue<String>();
+       // System.out.println(enames.remove());
+        System.out.println(enames.poll());        // output null
+    }
+    
+}
