c8861fd93cbca6f0199b76e5b241f0737dd5842b
siva krishna <sivakrishna053936@gmail.com>
2024-08-04 10:11:18 +0530

    test

--- OOP/SUPER CONCEPT IN DEFAULT CONSTRUCTOR/Flipkart.java
+++ OOP/SUPER CONCEPT IN DEFAULT CONSTRUCTOR/Flipkart.java
@@ -6,17 +6,22 @@ class Flipkart{
     this.discount=discount;
    }
 
-   Demo extends Flipkart{
+}
+
+  class Demo extends Flipkart{
         String item_name;
-        String brand_name
+        String brand_name;
         Demo(String item_name, String brand_name, int price, int discount){
-            Super(price,discount);
-            this.item_name=ite_name;
+            super(price,discount);
+            this.item_name=item_name;
             this.brand_name=brand_name;
         }
         public static void main(String[] args){
-            Demo d1=new Demo("shirt","denim",1999,599);
+            // Demo d1=new Demo("shirt","denim",1999,599);
+            Demo d1=new Demo("SHIRT", "LEVIS", 78955, 45);
             System.out.println(d1.discount);
+            Demo d2= new Demo("one","levis",4323,323);
+            System.out.println(d2);
+
         }
    }
-}
