461f71a535c126f6315df3242a742d6f5a29ca92
siva krishna <sivakrishna053936@gmail.com>
2024-08-04 17:30:59 +0530

    tset

---
+++ JAVA_PRACTICE/Identifiers/TestOne.java
@@ -0,0 +1,9 @@
+
+
+class Test{
+    int eid=105;
+    String ename="Sivakrishna";
+    public void m1(){};
+    public void m2(){};
+    String name="rahul";
+}
---
+++ JAVA_PRACTICE/Identifiers/demo.java
@@ -0,0 +1,13 @@
+
+
+public class demo extends Test {
+    public static void main(String[] args) {
+        demo d1=new demo();
+        d1.m1();
+        d1.m2();
+        System.out.println(d1.eid);
+        System.out.println(d1.ename);
+        System.out.println(d1.name);
+      
+    }
+}
