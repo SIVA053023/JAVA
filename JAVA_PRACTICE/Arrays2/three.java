cdb576350ae5c5fc498ee622757028094c64a286
siva krishna <sivakrishna053936@gmail.com>
2024-08-04 16:50:53 +0530

    test

--- JAVA_PRACTICE/Arrays2/three.java
+++ JAVA_PRACTICE/Arrays2/three.java
@@ -1,6 +1,6 @@
 class test{
     public static void main(String[] args) {
-        String[] enames={"Siva","Krishna","ujudfb","yhubgdhf","gyufgyu"};
+        String[] enames={"Siva","Krishna","ujudfb","yhubgdhf","gyufgyu","String"};
         for(String ename:enames){
             System.out.println(ename);
         }
