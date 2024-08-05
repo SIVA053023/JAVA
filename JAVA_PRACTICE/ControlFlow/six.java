85a5c02fd580d61055f305bc902a8c3349dfacf5
siva krishna <sivakrishna053936@gmail.com>
2024-08-05 11:44:21 +0530

    test

---
+++ JAVA_PRACTICE/ControlFlow/six.java
@@ -0,0 +1,13 @@
+
+public class six {
+    public static void main(String[] args) {
+        int [] eids={101,102,103,104,105,106};
+    // index          0   1   2   3   4   5        
+        int i=0;
+        while(i<=2){                             // here we are printing the indexing upto the 2 
+            System.out.println(eids[i]);          // output:--  101,102,103
+            i++;
+        }
+
+    }
+}
