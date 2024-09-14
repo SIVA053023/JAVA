66a38ebd3d43b2f593b6cd13bc7ade49ac35dea9
siva krishna <sivakrishna053936@gmail.com>
2024-09-14 14:18:51 +0530

    test

--- BI_Function/One.java
+++ BI_Function/One.java
@@ -1,9 +1,18 @@
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 
 class test{
     public static void main(String[] args) {
         BiFunction<String,String,Integer> obj=(fn,ln)->(fn+ln).length(); 
         System.out.println(obj.apply("rahul", "gandhi"));               // here it gives the lenth of the rahulgandi which is 11
+
+        BiFunction<String,String,String> uppercase=(a,b)->(a+b).toUpperCase();
+        System.out.println(uppercase.apply("Siva", "Krishna"));               // output= SIVAKRISHNA
+
+        BiPredicate<String,String> one=(a,b)->a.length()==b.length();
+        System.out.println(one.test("Siva", "krishna"));                   // output= false
+        
+
     }
 }
